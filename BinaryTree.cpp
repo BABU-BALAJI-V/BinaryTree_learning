5e275ca458bae818ccd880569634d79e82180524
babubalaji877841@gmail.com <babubalaji877841@gmail.com>
2023-09-24 00:30:29 +0530

    commit

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -1,37 +1,55 @@
 #include<iostream>
+#include<queue>
 using namespace std;
-class node
+class Node
 {
-    public:
-    int data;
-    node* left;
-    node* right;
-    node(int d)
-    {
-        this->data=d;
-        this->left=NULL;
-        this->right=NULL;
-    }
-
+	public:
+		int data;
+		Node* left;
+		Node* right;
+		Node(int d)
+		{
+			this->data=d;
+			this->left=NULL;
+			this->right=NULL;
+		}
 };
-node* buildTree(node* root)
+Node* buildTree(Node* root)
 {
-    int data;
-    cout<<"Enter the data: "<<endl;
-    cin>>data;
-    root=new node(data);
-    if(data==-1)
-    {
-        return NULL;
-    }
-    cout<<"Enter the left data: "<<endl;
-    root->left=buildTree(root->left);
-    cout<<"Enter the right data: "<<endl;
-    root->right=buildTree(root->right);
-    return root;
+	int data;
+	cout<<"Enter data: "<<endl;
+	cin>>data;
+	if(data==-1)
+	return NULL;
+	root=new Node(data);
+	cout<<"Enter left data: "<<endl;
+	root->left=buildTree(root->left);
+	cout<<"Enter right data: "<<endl;
+	root->right=buildTree(root->right);
+	return root;
+}
+void levelOrderTraversal(Node* root)
+{
+	queue<Node*> q;
+	q.push(root);
+	while(!q.empty())
+	{
+		Node*temp=q.front();
+		cout<<"Data: "<<temp->data<<" ";
+		q.pop();
+		if(temp->left)
+		{
+			q.push(temp->left);
+		}
+		if(temp->right)
+		{
+			q.push(temp->right);
+		}
+	}
 }
 int main()
 {
-    node* root=NULL;
-    root=buildTree(root);
+	Node*root=NULL;
+	root=buildTree(root);
+	levelOrderTraversal(root);
 }
